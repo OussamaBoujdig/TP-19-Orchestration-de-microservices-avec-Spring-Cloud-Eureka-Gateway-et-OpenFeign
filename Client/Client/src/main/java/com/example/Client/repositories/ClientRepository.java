450473138d98d6b40a78e8aae534a9263bba4731
OussamaBoujdig <oussamaboujdig8@gmail.com>
2025-12-12 19:42:04 +0100

    Delete Client directory

--- Client/Client/src/main/java/com/example/Client/repositories/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Client.repositories;
-
-import com.example.Client.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> { }
